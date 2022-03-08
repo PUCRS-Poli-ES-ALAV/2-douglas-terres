c41ca302382c068be081acdafbc212d370985fbc
Douglas <dodosobreira@gmail.com>
2022-03-07 21:01:05 -0300

    FEAT: Adicionando exercícios de 5 a 8

--- README.md
+++ README.md
@@ -9,6 +9,13 @@
 4. Modele e implemente um método recursivo que calcule
    o somatório dos números inteiros entre os números k e
    j, passados como parâmetro.
+5. Modele e implemente um método recursivo que recebeum String em retorna true se este String for um palíndrome, false caso contrário.
+   boolean isPal(String s)
+6. Modele e implemente um método recursivo que recebeum inteiro zero ou positivo e retorna um String com o número em binário.
+   String convBase2(int n)
+7. Modele e implemente um método recursivo que calcule o somatório dos números contidos em um ArrayList de inteiros, passado como parâmetro.
+8. Modele e implemente um método recursivo para encontrar o maior elemento de um ArrayList.
+   int findBiggest(ArrayList<Integer> ar)
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- src/App.java
+++ src/App.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class App
 {
     public static void main( String[] args )
     {
         App app = new App();
-        System.out.println(app.somatorio2(2, 5, 0));
+        // Testando todas as funções recursivas
+        System.out.println("Fatorial de 5: " + app.fatorial(5));
+        System.out.println("Somatorio de 5: " + app.somatorio(5, 0));
+        System.out.println("Fibonacci de 5: " + app.fibonacci(5));
+        System.out.println("Somatorio de 2 a 5: " + app.somatorio2(2, 5, 0));
+        System.out.println("Palindrome de \"arara\": " + app.isPal("arara"));
+        System.out.println("Conversão de 5 para binário: " + app.convBase2(5));
     }
 
    // implementar um método recursivo que calcula o fatorial de um número
@@ -40,7 +48,8 @@ public class App
    }
 
    // modele e implemente um método recursivo que calcule o somatorio dos numeros inteiros entre os numeros k e j
-   public static int somatorio2(int k, int j, int count)
+   // ex: somatorio2(2, 5, 0) = 2 + 3 + 4 = 9
+    public static int somatorio2(int k, int j, int count)
    {
        if (k == j) {
            return 0;
@@ -50,6 +59,70 @@ public class App
                k++;
            }
            return  k + somatorio2(k + 1, j, count + 1);
+       }
+   }
+
+   // Modele e implemente um método recursivo que recebeum String em retorna true se este String for umpalíndrome, false caso contrário.
+   // Ex: palindrome("arara") = true
+   // Ex: palindrome("casa") = false
+   public static boolean isPal(String s)
+   {
+       if (s.length() == 1) {
+           return true;
+       }
+       else {
+           if (s.charAt(0) == s.charAt(s.length() - 1)) {
+               return isPal(s.substring(1, s.length() - 1));
+           }
+           else {
+               return false;
+           }
+       }
+   }
+
+   //Modele e implemente um método recursivo que recebeum inteiro zero ou positivo e retorna um String com onúmero em binário. String convBase2(int n)
+   // Ex: convBase2(5) = "101"
+   public static String convBase2(int n)
+   {
+       if (n == 0) {
+           return "0";
+       }
+       else {
+           if (n % 2 == 0) {
+               return "0" + convBase2(n / 2);
+           }
+           else {
+               return "1" + convBase2(n / 2);
+           }
+       }
+   }
+
+   // Modele e implemente um método recursivo que calcule osomatório dos números contidos em um ArrayList deinteiros, passado como parâmetro.
+   // Ex: somatorioArray(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))) = 15
+   public static int somatorioArray(ArrayList<Integer> list)
+   {
+       if (list.size() == 0) {
+           return 0;
+       }
+       else {
+           return list.get(0) + somatorioArray((ArrayList<Integer>) list.subList(1, list.size()));
+       }
+   }
+
+   // Modele e implemente um método recursivo para encontrar o maior elemento de um ArrayList.  int findBiggest(ArrayList<Integer> ar)
+   // Ex: findBiggest(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5))) = 5
+   public static int findBiggest(ArrayList<Integer> ar)
+   {
+       if (ar.size() == 1) {
+           return ar.get(0);
+       }
+       else {
+           if (ar.get(0) > ar.get(1)) {
+               return findBiggest((ArrayList<Integer>) ar.subList(1, ar.size()));
+           }
+           else {
+               return findBiggest((ArrayList<Integer>) ar.subList(1, ar.size()));
+           }
        }
    }
 }
